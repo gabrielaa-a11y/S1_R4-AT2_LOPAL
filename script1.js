bbce0bac19151805780fff382637bc3b04932935
I1HSA <gabriela.a@edu.senai.br>
2026-04-13 11:27:10 -0300

    exercicio 2 concluido mostra valor do disconto e total

--- script1.js
+++
@@ -1,7 +0,0 @@
-let numero = prompt("Digite um número inteiro:");  //prompt pede o número, let guarda o valor
-
-if (numero % 2 == 0) {        //% 2 verifica se sobra 0
-    alert("O número é par.");    //alert mostra a resposta
-} else {                              //else serve para mostrar senão
-    alert("Número ímpar.");
-}
