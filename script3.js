fd8f2e56da9837a97e111033d67f58b13b77a317
I1HSA <gabriela.a@edu.senai.br>
2026-04-13 11:17:53 -0300

    todos os arquivos no mesmo repositorio

---
+++ script3.js
@@ -0,0 +1,7 @@
+let altura = prompt("Digite sua altura em metros:");  // prompt pede a altura, let guarda o valor
+
+if (altura >= 1.40) {         //if verifica
+    alert("Pode entrar no brinquedo.");       //alert mostra a mensagem
+} else {
+    alert("Altura insuficiente.");
+}
