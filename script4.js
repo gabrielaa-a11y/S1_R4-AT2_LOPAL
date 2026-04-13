2e85efc235ff0e634269084aec0a5b7c1662ac2f
I1HSA <gabriela.a@edu.senai.br>
2026-04-13 11:32:14 -0300

    exercicio 3 qual sua altura permite ou nao permite entrar no brinquedo

--- script4.js
+++
@@ -1,8 +0,0 @@
-let senha = prompt("Crie uma senha:");           //prompt pede a senha
-let confirmar = prompt("Digite a senha novamente:");      //let vai guarda o valor
-
-if (senha === confirmar) {   // if compara
-    alert("Acesso permitido.");    // alert mostra a resposta
-} else {
-    alert("Senha incorreta.");
-}
